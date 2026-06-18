100e00c1aa2d53292053bfbf524b80a25c60be1f
Łukasz Czyżyk <czyzykjunior@o2.pl>
2026-06-18 20:31:26 +0200

    Backend: przygotowanie modeli Register i Login pod ASP.NET Core Identity

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Techniki_Internetowe_Proj.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Adres e-mail jest wymagany.")]
+        [EmailAddress(ErrorMessage = "To nie jest poprawny adres e-mail.")]
+        [Display(Name = "Adres e-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Hasło must mieć co najmniej 6 znaków.")]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Podane hasła nie są identyczne.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
